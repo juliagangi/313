ec0ae13bbaa5debda5c50fa9d8736753443ccca0
juliagangi <97065262+juliagangi@users.noreply.github.com>
2023-05-08 19:30:59 -0500

    Update circuitcode.ino

--- circuitcode.ino
+++ circuitcode.ino
@@ -6,7 +6,7 @@
 void setup() {
   CircuitPlayground.begin();
   pinMode(13, OUTPUT);
-  pinMode(4, INPUT_PULLDOWN);
+  pinMode(4, INPUT);
   pinMode(5, INPUT_PULLDOWN);
   Serial.begin(9600);
 }
@@ -15,8 +15,9 @@ void loop() {
   digitalWrite(13, LOW);
   std::vector<std::vector<int>> plot;
   if (digitalRead(4)) {
+    Serial.println("in initial loop");
     int dose = 0;
-    int delay = 10000;
+    int currdelay = 10000;
     int currtime;
     bool noiseplaying = false;
     bool enterloop = false;
@@ -25,6 +26,7 @@ void loop() {
     int intensity;
     int numlights = 10;
     int prevlights = 10;
+    int timeelapsed;
     // play happy sound
     while (true) {
       if (digitalRead(5)) {
@@ -34,50 +36,60 @@ void loop() {
         if (digitalRead(4)) {
           enterloop = true;
         }
+      }
       if (digitalRead(4) || enterloop) {
-        dose = dose + 1;
-        intensity = .95-(dose*.05*abs(20000-timeelapsed)*prevlights/40000);
-        numlights = ceiling(intensity);
         // stop noise
-        if (dose == 1 or dose == 2) {
+        Serial.println("in loop");
+        dose = dose + 1;
+        timeelapsed = millis() - currtime;
+        intensity = .95-(dose*.05*abs(20000-timeelapsed)*prevlights/40000);
+        numlights = ceil(intensity);
+        if (dose < 3) {
           intensity = 100;
         }
         if (dose == 3) { // noise switches to annoying
           withdrawals = true;
           // re-assign noise
         }
-        for (i = 0; i < numlights; i++) {
+        for (int i = 0; i < numlights; i++) {
+          Serial.println("in lights loop");
+          uint8_t color1;
+          uint8_t color2;
+          uint8_t color3;
           if (i % 2 == 0) {
-            uint8_t color1 = 255;
-            uint8_t color2 = 255;
-            uint8_t color3 = 0;
+            color1 = 255;
+            color2 = 255;
+            color3 = 0;
           }
           else {
-            uint8_t color1 = 255;
-            uint8_t color2 = 16;
-            uint8_t color3 = 240;
+            color1 = 255;
+            color2 = 16;
+            color3 = 240;
           }
           CircuitPlayground.setPixelColor(i, color1, color2, color3);
         }
-        delay = delay - 1000; 
+        currdelay = currdelay - 1000; 
         currtime = millis(); 
         std::vector<int> vec(millis(),intensity);
         plot.push_back(vec);
         prevlights = numlights;
-        delay(2000);
-      }
-        for (i = 0; i < numlights; i++) {
+        delay(currdelay);
+        for (int i = 0; i < numlights; i++) {
           uint8_t color1 = 255;
           uint8_t color2 = 255;
           uint8_t color3 = 255;
           CircuitPlayground.setPixelColor(i, color1, color2, color3);
-        }      
-      if (millis() > currtime + delay) { // start noise
+        } 
+      }     
+      if (millis() > currtime + currdelay) { // start noise; don't need this if bc delaying inside if
         // play noise
+        Serial.println("noise starting");
         if (withdrawals) {
+          int x = 1;
           // play annoying noise
         }
         else {
+          int x = 1;
           // play happy noise
         }
         bool startnoise = true;
